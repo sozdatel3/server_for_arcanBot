b21bfb808a64a4d0e083c9dcd9079b356ecb4b40
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2024-08-23 00:23:49 +0300

    FIX (payment 11)

--- app/crud/db_city.py
+++ app/crud/db_city.py
@@ -319,8 +319,12 @@ def check_signature(transaction_id, signature_value) -> bool:
         if result:
             user_id = result["user_id"]
             amount = result["amount"]
+            print(
+                "HERE:\n"
+                f"{amount}:{transaction_id}:{settings.ACTIVE_ROBOKASSA_PASSWORD2}:Shp_id= {user_id}\n"
+            )
             check_signature = hashlib.md5(
-                f"{amount}:{transaction_id}:{settings.ACTIVE_ROBOKASSA_PASSWORD2}:Shp_id= {user_id}".encode()
+                f"{amount}:{transaction_id}:{settings.ACTIVE_ROBOKASSA_PASSWORD2}:Shp_id= {user_id} :".encode()
             ).hexdigest()
             if check_signature == signature_value:
                 print("GOOOOD CHECK")
